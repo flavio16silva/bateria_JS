c375e5e481b23d774f771e9528e60a2d15ab533c
Flavio Silva <fal_silva@hotmail.com>
2024-11-05 11:06:01 -0300

    script projeto bateria

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//------------------ EVENTOS --------------------
 //Reconhecendo as teclas em qualquer local do site:
 //Representa o corpo do site - document.body
 //Observador de eventos - addEventListener 
@@ -7,9 +8,52 @@ document.body.addEventListener('keyup', (evento) => {
     //console.log(evento.code.toLowerCase())
 })
 
+//JS irá escutar ao click no botão e executar uma função
+document.querySelector('.composer button').addEventListener('click', () => {
+    let som = document.querySelector('#input').value 
+    //console.log('Musica', som)
+
+    if (som !== ''){ //transformando a string em array
+        let somArray = som.split('')
+        playComposition(somArray) //Função a ser chamada
+        //console.log(somArray)
+    } else {
+        console.log('Digite um som!') //MELHORAR
+    }
+})
 
 
-//Função criada para ao tocar na tecla executar um som:
+// ------------------- FUNÇÕES ----------------------
+//Função para ao tocar na tecla executar um som:
 function playSound(sound) {
-    let audioElemento = document.querySelector(`#s_${sound}`) //pegando as teclas dinamicamente
+    let audioElemento = document.querySelector(`#s_${sound}`) //pega as teclas dinamicamente(SOM)
+    let keyElemento = document.querySelector(`div[data-key="${sound}"]`) //seleciona a key ou componente da tecla que foi digitada
+    
+    
+    if(audioElemento){
+        audioElemento.currentTime = 0 //retorna o ponteiro do som para o começo ao teclar
+        audioElemento.play() //executa o som pelas teclas cadastradas
+    }
+
+    if(keyElemento){
+        keyElemento.classList.add('active') //adiciona a cor pelo valor ativo ao teclar
+
+        setTimeout(() => {
+            keyElemento.classList.remove('active') //remove a cor pelo valor ativo ao sair da tecla
+        }, 300)
+    }
+
+}
+
+//Função para tocar a lista de teclas digitadas
+function playComposition(somArray){
+    let esperar = 0
+
+    for(let somItem of somArray){
+        setTimeout(() => {          //criar intervalos a cada som emitido
+            playSound(`key${somItem}`)
+        }, esperar)
+
+        esperar += 250
+    }
 }
